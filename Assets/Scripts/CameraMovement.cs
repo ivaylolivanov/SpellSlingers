2886666d91d887df932b0e76bb316e6f3bc99ee5
ivaylolivanov <ivaylolivanov95@gmail.com>
2020-12-17 17:51:19 +0200

    Add camera movement script focusing the player
    Signed-off-by: ivaylolivanov <ivaylolivanov95@gmail.com>

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+    [SerializeField] private Transform target;
+    [SerializeField] private float smoothSpeed = 0.125f;
+    [SerializeField] private Vector3 offset;
+
+    void FixedUpdate() {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(
+            transform.position,
+            desiredPosition,
+            smoothSpeed * Time.deltaTime
+        );
+        transform.position = smoothedPosition;
+
+        transform.LookAt(target);
+    }
+}
