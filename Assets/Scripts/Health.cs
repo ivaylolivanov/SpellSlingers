bc25639be18c217d678b1248c4aa859f8dfeea8f
ivaylolivanov <ivaylolivanov95@gmail.com>
2020-12-19 01:53:26 +0200

    Add Health.cs
    Signed-off-by: ivaylolivanov <ivaylolivanov95@gmail.com>

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+    [SerializeField] private int hitPoints = 100;
+
+    void Update() {
+        if(hitPoints <= 0) {
+            Die();
+        }
+    }
+
+    public void DoDamage(int amount) {
+        hitPoints -= amount;
+    }
+
+    public void Restore(int amount) {
+        hitPoints += amount;
+    }
+
+    private void Die() {
+        // Play VFX
+        // Play SFX
+        Destroy(gameObject);
+    }
+}
