e0b41d92eb85ab8e1857a8057bd1c78a610e3960
ivaylolivanov <ivaylolivanov95@gmail.com>
2020-12-19 01:53:39 +0200

    Add enemy script
    Signed-off-by: ivaylolivanov <ivaylolivanov95@gmail.com>

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private int hitPoints = 100;
+
+    void Update() {
+        if(hitPoints <= 0) {
+            Die();
+        }
+    }
+
+    void Die() {
+        // Play VFX
+        // Play SFX
+        Destroy(gameObject);
+    }
+}
