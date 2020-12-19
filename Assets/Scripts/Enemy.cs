6501236b5b1954a5edefc0f296ecf50b58bc8327
ivaylolivanov <ivaylolivanov95@gmail.com>
2020-12-19 09:40:13 +0200

    Enemy.cs - responsible for the enemy behavior
    Signed-off-by: ivaylolivanov <ivaylolivanov95@gmail.com>

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,19 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
-{
-    [SerializeField] private int hitPoints = 100;
+public class Enemy : MonoBehaviour {
+    [SerializeField] private float speed = 3f;
 
-    void Update() {
-        if(hitPoints <= 0) {
-            Die();
-        }
+    private Rigidbody2D rb;
+    private Vector2 destination;
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    void Die() {
-        // Play VFX
-        // Play SFX
-        Destroy(gameObject);
+    void OnTriggerStay2D(Collider2D col) {
+        if(col.tag == "Player") {
+            Vector2 playerLastSeenPosition = col.gameObject.transform.position;
+            Vector2 lookDir = playerLastSeenPosition - rb.position;
+            float lookAngleRads = Mathf.Atan2(lookDir.y, lookDir.x);
+            float lookAngleDirDeg = lookAngleRads * Mathf.Rad2Deg;
+            float lookDirDegPOV = lookAngleDirDeg - 90f;
+
+            rb.rotation = lookDirDegPOV;
+
+            transform.position = Vector2.MoveTowards(
+                transform.position,
+                playerLastSeenPosition,
+                speed * Time.deltaTime
+            );
+        }
     }
 }
