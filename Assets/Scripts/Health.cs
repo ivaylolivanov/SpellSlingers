cfb5af9591fbae68533b35e4eedf0690dbe5321d
ivaylolivanov <ivaylolivanov95@gmail.com>
2020-12-20 20:26:55 +0200

    Health.cs updates also the health bar, given to it
    Signed-off-by: ivaylolivanov <ivaylolivanov95@gmail.com>

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,14 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour {
     [SerializeField] private int maxHitPoints = 100;
+    [SerializeField] private GameObject healthBar;
 
     private int currentHitPoints;
+    private Slider healthBarSlider;
 
     void Start() {
         currentHitPoints = maxHitPoints;
+        if(healthBar) {
+            healthBarSlider = healthBar.GetComponent<Slider>();
+            healthBarSlider.maxValue = maxHitPoints;
+            healthBarSlider.value = currentHitPoints;
+        }
     }
 
     void Update() {
@@ -19,6 +27,9 @@ public class Health : MonoBehaviour {
 
     public void DoDamage(int amount) {
         currentHitPoints -= amount;
+        if(healthBar) {
+            healthBarSlider.value = currentHitPoints;
+        }
     }
 
     public void Restore(int amount) {
