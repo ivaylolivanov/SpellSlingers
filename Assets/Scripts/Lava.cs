9da7221d104293238bfbfdbc8bc81266b32ac502
ivaylolivanov <ivaylolivanov95@gmail.com>
2020-12-26 22:51:35 +0200

    Script to define the behavior of the lava tiles
    Signed-off-by: ivaylolivanov <ivaylolivanov95@gmail.com>

---
+++ Assets/Scripts/Lava.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lava : MonoBehaviour {
+    [SerializeField] private int damage = 1;
+    [SerializeField] private float timeBetweenDamage = 2f;
+
+    private bool waiting = false;
+    void OnTriggerStay2D(Collider2D coll) {
+        if(! waiting) {
+            waiting = true;
+            StartCoroutine(Damage(coll));
+        }
+    }
+
+    private IEnumerator Damage(Collider2D coll) {
+        Health healthComponent = coll.GetComponent<Health>();
+        if(healthComponent != null) {
+            healthComponent.DoDamage(damage);
+            yield return new WaitForSeconds(timeBetweenDamage);
+            waiting = false;
+        }
+    }
+}
