16f8ea6f315685e0ed3da0c155154d05dc1b0975
ivaylolivanov <ivaylolivanov95@gmail.com>
2021-01-09 20:52:55 +0200

    Add Pause / resume mechanism and menu prefab
    Signed-off-by: ivaylolivanov <ivaylolivanov95@gmail.com>

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+    private static bool isGamePaused = false;
+
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.Escape)) {
+            isGamePaused = !isGamePaused;
+        }
+
+        if(isGamePaused) {
+            Pause();
+        }
+        else {
+            Resume();
+        }
+    }
+
+    private void Pause() {
+        Time.timeScale = 0f;
+    }
+
+    private void Resume() {
+        Time.timeScale = 1f;
+    }
+}
