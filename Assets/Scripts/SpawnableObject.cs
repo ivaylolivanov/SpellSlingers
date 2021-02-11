36aaba16db4adcd73b75f1f221cc2dbe34b7660b
ivaylolivanov <ivaylolivanov95@gmail.com>
2021-02-11 13:30:21 +0200

    Add abstract new class for spawnable objects
    Signed-off-by: ivaylolivanov <ivaylolivanov95@gmail.com>

---
+++ Assets/Scripts/SpawnableObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class SpawnableObject : MonoBehaviour {
+    public string objectName = "New Object";
+
+    public abstract void SetForce(float force);
+    public abstract void SetRange(float range);
+    public abstract void Die(float delay = 0);
+}
